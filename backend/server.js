8e97459b5fd61caa5def9b74c1e9738a7ece50a8
Mizanur Rahman <nickty.86@gmail.com>
2021-02-08 21:40:34 +0600

    Database connection done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const app = require('./app')
 
+const connectDatabase = require('./config/database')
+
 const dotenv = require('dotenv')
 //setting up config file 
 dotenv.config({path: 'backend/config/config.env'})
+
+//connection to dabase 
+connectDatabase();
 
 app.listen(process.env.PORT, () => console.log(`Server started on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode `))
