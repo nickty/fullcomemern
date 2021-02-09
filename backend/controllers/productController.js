1d5c0541eeba97b9163e2070ed9653ecc199a3c5
Mizanur Rahman <nickty.86@gmail.com>
2021-02-09 08:18:07 +0600

    creating new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,18 @@
+const { baseModelName } = require("../models/product")
+
+const Product = require('../models/product')
+
+//Create New product => /api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+
+    const product = await Product.create(req.body)
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
 exports.getProduct = (req, res, next) => {
     res.status(200).json({
         success: true,
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { getProduct } = require('../controllers/productController')
+const { getProduct, newProduct } = require('../controllers/productController')
+
 const router = express.Router()
 
 
+router.route('/product/new').post(newProduct)
 
 router.route('/products').get(getProduct)
 
