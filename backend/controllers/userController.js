6de841d9ea9b4c517b22cb539a67644487f60935
Mizanur Rahman <nickty.86@gmail.com>
2021-02-13 08:36:26 +0600

    cookie generated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const sendToken = require('../utils/jwtToken')
 
 //Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors (async (req, res, next) =>{
@@ -49,10 +50,12 @@ exports.loginUser = catchAsyncErrors (async (req, res, next) => {
         return next(new ErrorHandler('Invalied or password', 401))
     }
 
-    const token = user.getJwtToken()
+    // const token = user.getJwtToken()
 
-    res.status(200).json({
-        success: true,
-        token
-    })
+    // res.status(200).json({
+    //     success: true,
+    //     token
+    // })
+
+    sendToken(user, 200, res)
 })
