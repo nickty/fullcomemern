b31e2561fa79ecde991ac81881989937c0d34969
Mizanur Rahman <nickty.86@gmail.com>
2021-02-15 23:00:37 +0600

    footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Footer />
     </div>
   );
 }
