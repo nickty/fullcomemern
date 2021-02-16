c1b789c968fa8a28f3d5f2c57317d692de63352d
Mizanur Rahman <nickty.86@gmail.com>
2021-02-16 17:26:42 +0600

    seperate product component from home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Fragment, useEffect } from 'react'
 import MetaData from './layout/MetaData'
 
+import Product from './product/Product'
+
 import {useDispatch, useSelector} from 'react-redux'
 import {getProducts} from '../actions/productActions'
 
@@ -24,30 +26,8 @@ const Home = () => {
             <section id="products" className="container mt-5">
                 <div className="row">
                     {products && products.map(product => (
-                        <div key={product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
-                         <div className="card p-3 rounded">
-                             <img
-                             className="card-img-top mx-auto"
-                             src={product.images[0].url}
-                             />
-                             <div className="card-body d-flex flex-column">
-                             <h5 className="card-title">
-                                 <a href="">{product.name}</a>
-                             </h5>
-                             <div className="ratings mt-auto">
-                                 <div className="rating-outer">
-                                 <div className="rating-inner" style={{with: `${(product.rating / 5) *100}%`}}>
-
-                                 </div>
-                                 </div>
-                                 <span id="no_of_reviews">({product.numberOfReviews} Reviews)</span>
-                             </div>
-                             <p className="card-text">${product.price}</p>
-                             <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-                             </div>
-                         </div>
-                        </div>
-                    ))}
+                        <Product key={product._id} product={product} />
+                     ))}
                    
                
             </div>
