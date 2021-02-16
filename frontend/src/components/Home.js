a324d7ec68080cb7e92f4eeceeca0a2263335694
Mizanur Rahman <nickty.86@gmail.com>
2021-02-16 17:37:24 +0600

    apply text loading

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -19,19 +19,24 @@ const Home = () => {
     return (
         <Fragment>
 
-            <MetaData title={'Buy best products online'} />
+            {loading ? <h1>Loading...</h1> : (
+                <Fragment>
+                    <MetaData title={'Buy best products online'} />
 
-            <h1 id="products_heading">Latest Products</h1>
+                    <h1 id="products_heading">Latest Products</h1>
 
-            <section id="products" className="container mt-5">
-                <div className="row">
-                    {products && products.map(product => (
-                        <Product key={product._id} product={product} />
-                     ))}
-                   
-               
-            </div>
-            </section>
+                    <section id="products" className="container mt-5">
+                        <div className="row">
+                            {products && products.map(product => (
+                                <Product key={product._id} product={product} />
+                            ))}
+                        
+                    
+                    </div>
+                    </section>
+                </Fragment>
+            )}
+        
             
         </Fragment>
     )
