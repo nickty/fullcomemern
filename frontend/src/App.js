ea61654cf7f702eb8c89283e11c50cbcc5d2f82c
Mizanur Rahman <nickty.86@gmail.com>
2021-02-17 08:13:10 +0600

    display product details html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
 
+import ProductDetails from './components/product/ProductDetails'
+
 function App() {
   return (
     <Router>
@@ -12,6 +14,7 @@ function App() {
             <Header />
               <div className="container container-fluid">
                   <Route path="/" component={Home} exact />
+                  <Route path="/product/:id" component={ProductDetails} exact />
               </div>
              
             <Footer />
