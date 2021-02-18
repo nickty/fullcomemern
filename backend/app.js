fa4708362f5dea1fc84c33fc8a0eaf26014de137
Mizanur Rahman <nickty.86@gmail.com>
2021-02-18 07:24:13 +0600

    adding cloudinary for images

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,18 @@ const app = express()
 
 const errorMiddleware = require('./middlewares/errors')
 const cookieParser = require('cookie-parser')
+const cloudinary = require('cloudinary')
+
 
 app.use(express.json())
 app.use(cookieParser())
+
+//Setting cloudinary
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const products = require('./routes/product')
 const users = require('./routes/user')
