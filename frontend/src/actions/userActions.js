3bf71450f8d3d5733fb01a6fd12f275df9096c6a
Mizanur Rahman <nickty.86@gmail.com>
2021-02-18 06:59:16 +0600

    register user reducer and action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL, CLEAR_ERRORS} from '../constants/userConstants'
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL, CLEAR_ERRORS, REGISTER_USER_REQUEST, PRODUCT_DETAILS_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_SUCCESS} from '../constants/userConstants'
 import axios from 'axios'
 
 //Login
@@ -27,6 +27,34 @@ export const login = (email, password) => async (dispatch) => {
         })
     }
 }
+
+//register user 
+export const register = (userData) => async (dispatch) => {
+    try {
+
+        dispatch({type:REGISTER_USER_REQUEST})
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const {data} = await axios.post('/api/v1/register', {userData}, config)
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+         
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.errMessage
+        })
+    }
+}
+
 
 
 //Clear Errors 
