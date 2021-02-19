61a0b394935a60a123f8fe183f2082c499fd7b4d
Mizanur Rahman <nickty.86@gmail.com>
2021-02-19 07:31:15 +0600

    logout user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,7 +8,9 @@ import {
     REGISTER_USER_SUCCESS,
     LOAD_USER_REQUEST, 
     LOAD_USER_SUCCESS, 
-    LOAD_USER_FAIL
+    LOAD_USER_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL
 } from '../constants/userConstants'
 import axios from 'axios'
 
@@ -93,6 +95,25 @@ export const loadUser = () => async (dispatch) => {
     }
 }
 
+
+//Logout User 
+export const logout = () => async (dispatch) => {
+
+    try {
+
+        await axios.get('/api/v1/logout')
+
+        dispatch({
+            type: LOGOUT_SUCCESS
+        })
+         
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response.data.errMessage
+        })
+    }
+}
 
 
 //Clear Errors 
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,7 +8,9 @@ import {
     REGISTER_USER_FAIL, 
     LOAD_USER_REQUEST, 
     LOAD_USER_SUCCESS, 
-    LOAD_USER_FAIL
+    LOAD_USER_FAIL, 
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL
 }    from '../constants/userConstants'
 
 export const userReducer = (state = {user:{}}, action) => {
@@ -29,15 +31,27 @@ export const userReducer = (state = {user:{}}, action) => {
                 isAuthenticated: true,
                 user: action.payload
             } 
+        case LOGOUT_SUCCESS:
+            return {
+                loading: false,
+                isAuthenticated: false,
+                user: null
+            }    
         case LOAD_USER_FAIL: 
             return {
             loading: false, 
             isAuthenticated: false, 
             user: null, 
             error: action.payload
-        }       
+        }  
+        
+        case LOGOUT_FAIL: 
+            return {
+                ...state,
+                error: action.payload
+            }
         case LOGIN_FAIL:
-        case REGISTER_USER_FAIL:     
+        case REGISTER_USER_FAIL:    
             return {
                 ...state,
                 loading: false,
