d74e732be03583107c7a1069cc29d4d5413f00de
Mizanur Rahman <nickty.86@gmail.com>
2021-03-08 22:13:16 +0600

    files prepareation before deploy

---
+++ Readme.md
@@ -0,0 +1,1 @@
+> Omcplete E-commerce site build with Nodejs, React, Redux, Express, MongoDB
--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ process.on('uncaughtException', err=> {
 })
 
 //setting up config file 
-dotenv.config({path: 'backend/config/config.env'})
+if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').dotenv.config({path: 'backend/config/config.env'})
 
 //connection to dabase 
 connectDatabase();
