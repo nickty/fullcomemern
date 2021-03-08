70fa08b124363cbd9f89f14331d52bbead88a250
Mizanur Rahman <nickty.86@gmail.com>
2021-03-08 21:48:01 +0600

    joint backend and frontend into one

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const fileupload = require('express-fileupload')
 const dotenv = require('dotenv')
+const path = require('path')
 
 
 //setting up config file 
@@ -25,6 +26,14 @@ app.use('/api/v1', products)
 app.use('/api/v1', users)
 app.use('/api/v1', order)
 app.use('/api/v1', payment)
+
+if(process.env.NODE_ENV === 'PRODUCTION'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 //Middlewares to handle error
 app.use(errorMiddleware)
